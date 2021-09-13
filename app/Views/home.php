3a1430ba9d89a11f2b207df946fb2eaad07db65c
didikkoetoe <danieldidikprabowo@gmail.com>
2021-09-13 12:29:47 +0700

    View home

---
+++ app/Views/home.php
@@ -0,0 +1,13 @@
+<?= $this->extend('layout/default'); ?>
+<?= $this->section('title'); ?>
+<title>Home &mdash; yukNikah</title>
+<?= $this->endSection(); ?>
+<?= $this->section('content'); ?>
+<section class="section">
+    <div class="section-header">
+        <h1>Dashboard</h1>
+    </div>
+    <div class="section-body">
+    </div>
+</section>
+<?= $this->endSection(); ?>
