194c93ec4e12bed21a9e4acaf8903db64734f05c
didikkoetoe <danieldidikprabowo@gmail.com>
2021-09-20 16:59:08 +0700

    Menambahkan method edit dan update untuk menangani update data

--- app/Controllers/Gawe.php
+++ app/Controllers/Gawe.php
@@ -34,4 +34,36 @@ class Gawe extends BaseController
 			return redirect()->to(site_url('gawe'))->with('success', 'Data berhasil disimpan.');
 		}
 	}
+
+	public function edit($id = null)
+	{
+		if($id != null) {
+			$query = $this->db->table('gawe')->getWhere(['id_gawe' => $id]);
+			if($query->resultID->num_rows > 0) {
+				$data['gawe'] = $query->getRow();
+				return view('gawe/edit', $data);
+			} else {
+			throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound('Data gawe tidak ditemukan');
+			}
+		} else {
+			throw \CodeIgniter\Exceptions\PageNotFoundException::forPageNotFound('File tidak ditemukan');
+		}
+	}
+
+	public function update($id)
+	{
+		// Cara 1 : Request sesui table di DB
+		// $data = $this->request->getPost();
+		// unset($data['_method']);
+
+		// Cara 2 : Data spesifik
+		$data = [
+			'name_gawe' => $this->request->getPost('name_gawe'),
+			'date_gawe' => $this->request->getPost('date_gawe'),
+			'info_gawe' => $this->request->getPost('info_gawe'),
+		];
+
+		$this->db->table('gawe')->where(['id_gawe' => $id])->update($data);
+		return redirect()->to(site_url('gawe'))->with('success' , 'Data berhasil di update.');
+	}
 }
